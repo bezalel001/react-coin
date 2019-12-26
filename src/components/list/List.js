3e5a8394f920f8036ecb1edfdd665bf2eec56989
bezalel001 <mickeybezalel@gmail.com>
2019-12-26 18:23:24 +0100

    react coin

---
+++ src/components/list/List.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import { handleResponse , renderChangePercent} from '../../helpers';
+import { API_URL } from '../../config';
+import Loading from '../common/Loading';
+import Table from './Table';
+import Pagination from './Pagination';
+
+class List extends React.Component {
+    constructor(){
+        super();
+
+        this.state = {
+            loading: false,
+            currencies: [], // populate this sith data received from server
+            error: null, // update this if server returns some error message
+            totalPages: 0,
+            page: 1,
+        }
+
+        this.handlePaginationClick = this.handlePaginationClick.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchCurrencies();
+    }
+
+    fetchCurrencies() {
+        this.setState({ loading: true });
+        const {page} = this.state;
+        fetch(`${API_URL}/cryptocurrencies?page=${page}&perPage=20`)
+        .then(handleResponse)
+        .then((data) => {
+          const {currencies, totalPages } = data;
+          this.setState({ 
+              currencies: currencies, 
+              totalPages: totalPages, 
+              loading: false });
+        })
+        .catch((error) => {
+          this.setState({ error: error.errorMessage, loading: false });
+          
+        });
+    }
+
+    
+
+    handlePaginationClick(direction) {
+        let nextPage = this.state.page;
+        nextPage = direction === 'next' ? nextPage + 1 : nextPage - 1;
+        this.setState({ page: nextPage }, () => {
+            this.fetchCurrencies();
+        });
+    }
+
+    render(){
+        const { loading, error, currencies, page, totalPages } = this.state;
+
+        // render only loading component, if loading state is set to true
+        if (loading) {
+            return <div className="loading-container"><Loading /></div>
+        }
+        // render only error message, if error occurred while fetching data
+        if (error) {
+            return <div className="error">{error}</div>
+        }
+
+        
+        return (
+            <div>
+                <Table 
+                    currencies={currencies} 
+                />
+                <Pagination 
+                    page={page}
+                    totalPages={totalPages}
+                    handlePaginationClick={this.handlePaginationClick}
+                />
+            </div>
+        )
+    }
+}
+export default List;
